62c3fbc37178f8bf60697c550fb7a7bd8ffba830
fratim <frtim@student.ethz.ch>
2022-05-13 16:31:05 +0100

    fig v2 gym created

--- plot_hopper_from_cheetah.py
+++ plot_hopper_from_cheetah.py
@@ -7,29 +7,42 @@ matplotlib.use("pgf")
 matplotlib.rcParams.update({
     "pgf.texsystem": "pdflatex",
     'font.family': 'serif',
-    'font.size': 20,
+    'font.size': 15,
     'text.usetex': True,
     'pgf.rcfonts': False,
 })
 
-DATA_PATH = "/Users/tim/Data/nips_figures"
-PROJECT = "Gym"
-LEARNER = "hopper"
-DEM = "half_cheetah"
 
-data_in = load_data(DATA_PATH, PROJECT, LEARNER, DEM)
-data_to_plot = process_data(data_in)
+FIG_SIZE = (5, 5)
 
-fig, ax = plt.subplots(constrained_layout=True)
-plt.show()
+def make_and_save_figure(data_path, fig_save_path, learner, expert):
+    data_in = load_data(data_path, learner, expert)
+    data_to_plot = process_data(data_in)
 
-plot_data(data_to_plot, ax)
+    fig, ax = plt.subplots(figsize=FIG_SIZE)
+    plt.show()
 
-ax.set_title(f"Exp: {DEM} - Learner: {LEARNER} - Seeds: {data_to_plot['n_seeds']}")
-ax.set_xlabel("Environment Steps")
-ax.set_ylabel("Distance Travelled")
+    plot_data(data_to_plot, ax)
 
-save_fig(plt, data_to_plot, DATA_PATH, PROJECT, LEARNER, DEM)
+    ax.set_title(f"Learner:{learner}-Exp:{expert}-Seeds:{data_to_plot['n_seeds']}")
+    # ax.set_xlabel("Environment Steps")
+    # ax.set_ylabel("Distance Travelled")
 
-print("Done")
+    save_fig(plt, data_to_plot, fig_save_path, learner, expert)
+
+DATA_PATH = "/Users/tim/Data/nips_figures/Gym/wandb_exported_v1"
+
+FIGURE_PATH_OUT = "/Users/tim/Data/nips_figures/Gym"
+FIGURE_VERSION = "v2"
+fig_save_path = os.path.join(FIGURE_PATH_OUT, FIGURE_VERSION)
+os.makedirs(fig_save_path, exist_ok=True)
+
+# embodiments = ["Hopper", "Walker", "HalfCheetah"]
+#
+# for expert in embodiments:
+#     for learner in embodiments:
+#         if expert == learner:
+#             continue
+#         make_and_save_figure(DATA_PATH, fig_save_path, learner, expert)
+
 
--- utils.py
+++ utils.py
@@ -2,29 +2,53 @@ import os
 import pandas
 import numpy as np
 
-def get_data_dir(DATA_PATH, PROJECT, LEARNER):
-    return os.path.join(DATA_PATH, PROJECT, LEARNER)
+X_MAX = 2500000
+SMOOTHING = 5
 
-def get_fname(LEARNER, DEM):
-    fname = f"{LEARNER}_from_{DEM}"
+def find_cutoff(x):
+    cutoff = -1
+    for i in range(len(x)):
+        if x[i+1] > X_MAX:
+            cutoff = i
+            break
+    if cutoff == -1:
+        raise ValueError
+
+    return cutoff
+
+def smooth_data(y):
+    assert SMOOTHING%2 == 1
+    offset = int((SMOOTHING-1)/2)
+
+    for i in range(offset, len(y)-offset-1):
+        summed = 0
+        for j in np.arange(-1*offset, offset+1):
+            summed += y[int(i+j)]
+        average = summed/SMOOTHING
+        y[i] = average
+
+    return y
+
+
+def get_fname_input(learner, expert):
+    name = f"{learner}from{expert}"
+    return name
+
+def get_fname_output(learner, expert):
+    fname = f"{learner}_from_{expert}"
     return fname
 
-def get_figure_fpath(data_to_plot, DATA_PATH, PROJECT, LEARNER, DEM):
-    data_dir = get_data_dir(DATA_PATH, PROJECT, LEARNER)
-    fname_base = get_fname(LEARNER, DEM)
-    fname = f"{fname_base}_nseeds{data_to_plot['n_seeds']}"
+def get_figure_fpath(data_to_plot, fig_save_path, learner, expert):
+    fname_base = get_fname_output(learner, expert)
+    fname = f"{fname_base}"
 
-    fpath = os.path.join(data_dir, fname)
+    fpath = os.path.join(fig_save_path, fname)
 
     return fpath
 
-def load_data(DATA_PATH, PROJECT, LEARNER, DEM):
-
-    data_dir = get_data_dir(DATA_PATH, PROJECT, LEARNER)
-    fname = get_fname(LEARNER, DEM) + ".csv"
-
-    fpath = os.path.join(data_dir, fname)
-
+def load_data(data_path, learner, expert):
+    fname = get_fname_input(learner, expert) + ".csv"
+    fpath = os.path.join(data_path, fname)
     data_in = pandas.read_csv(fpath)
 
     assert (data_in.shape[1] - 1) % 3 == 0
@@ -50,8 +74,16 @@ def get_xy_pairs(data):
     x_y_pairs = []
 
     for seed in range(n_seeds):
+
         x = data.iloc[:, 0].values
         y = data.iloc[:, 4 + 6 * seed].values
+
+        y = smooth_data(y)
+
+        cutoff = find_cutoff(x)
+
+        x = x[:cutoff]
+        y = y[:cutoff]
 
         if len(x_y_pairs) > 0:
             assert x.shape == x_y_pairs[-1][0].shape
@@ -93,7 +125,8 @@ def plot_data(data_to_plot, ax):
         mean = data_to_plot["mean"]
         ax.plot(x, mean, color="blue")
 
-def save_fig(plt, data_to_plot, DATA_PATH, PROJECT, LEARNER, DEM):
-    fpath_base = get_figure_fpath(data_to_plot, DATA_PATH, PROJECT, LEARNER, DEM)
+def save_fig(plt, data_to_plot, fig_save_path, learner, expert):
+    fpath_base = get_figure_fpath(data_to_plot, fig_save_path, learner, expert)
+    plt.tight_layout()
     plt.savefig(f"{fpath_base}.png")
     plt.savefig(f"{fpath_base}.pgf")
