6a9fd06f7b5e8a9da8dcbeb29106f5fff00b45b7
fratim <frtim@student.ethz.ch>
2022-05-18 11:18:41 +0100

    code used to plot ALL figures

--- make_plots.py
+++ make_plots.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 import os
 import pandas
+
+import utils
 from utils import load_data, process_data, plot_data, get_figure_fpath, save_fig
 import matplotlib
+import numpy as np
+
 matplotlib.use("pgf")
 matplotlib.rcParams.update({
     "pgf.texsystem": "pdflatex",
@@ -12,48 +16,107 @@ matplotlib.rcParams.update({
     'pgf.rcfonts': False,
 })
 
-COLORS = ["red", "blue"]
-FIG_SIZE = (5, 5)
+ABLATION_IDS = ["abl_nonTI", "abl_noembed", "abl_largerembed", "abl_singletraj"]
 
-def make_and_save_figure(data_path, fig_save_path, learner, expert, algos):
+COLORS = ["red", "blue"]
+ABL_COLORS = ["red", "green", "blue"]
+FIG_SIZE = (4, 3)
+
+def make_and_save_figure(data_path, fig_save_path, learner, expert, algos, is_ablation=False):
     fig, ax = plt.subplots(figsize=FIG_SIZE)
-    plt.show()
+
+    # draw and show it
+    fig.canvas.draw()
+    plt.show(block=False)
+
+    y_margin_max = np.NINF
+
+    data_to_plot_collected = []
+
+    if not is_ablation:
+        title = f"Learner:{learner}-Exp:{expert}"
+        colors = COLORS
+    else:
+        title = f"Learner:{learner}-Exp:{expert}-{algos[1]}"
+        colors = ABL_COLORS
+
+    if algos[1] == "abl_singletraj":
+        algos.append("baseline")
 
     for i, algo in enumerate(algos):
+        print(f"Loading data for {learner} from {expert} - {algo}")
         data_in = load_data(data_path, learner, expert, algo)
-        data_to_plot = process_data(data_in)
+        data_to_plot = process_data(data_in, agent_type=learner)
+        y_margin_max = data_to_plot["y_margin_max"] if data_to_plot["y_margin_max"] > y_margin_max else y_margin_max
+        data_to_plot_collected.append(data_to_plot)
 
-        plot_data(data_to_plot, ax, color=COLORS[i])
+    for i, algo in enumerate(algos):
+        plot_data(data_to_plot_collected[i], ax, color=colors[i], label=algo, fig=fig)
 
+    ax.set_title(title)
+    # ax.set_xlabel("Environment Steps")
+    # ax.set_ylabel("Reward")
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    # plt.legend(frameon=False, loc=4)
 
-    ax.set_title(f"Learner:{learner}-Exp:{expert}-Seeds:{data_to_plot['n_seeds']}")
-    ax.set_xlabel("Environment Steps")
-    ax.set_ylabel("Reward")
+    if learner not in ["gripper", "longstick"]:
+        ax.set_xticks([0, 2500000])
+    elif learner == "gripper":
+        ax.set_xlim([0, 800000])
+        ax.set_xticks([0, 800000])
+    else:
+        ax.set_xlim([0, 120000])
+        ax.set_xticks([0, 120000])
 
-    save_fig(plt, data_to_plot, fig_save_path, learner, expert)
+    if not is_ablation:
+        save_fig(plt, fig_save_path, learner, expert)
+    else:
+        save_fig(plt, fig_save_path, learner, expert, ending=algos[1])
+
+DATA_PATH = "/Users/tim/Data/nips_figures/"
 
 FIGURE_PATH_OUT = "/Users/tim/Data/nips_figures/"
-
-FIGURE_VERSION = "v2"
-fig_save_path = os.path.join(FIGURE_PATH_OUT, FIGURE_VERSION)
+fig_save_path = os.path.join(FIGURE_PATH_OUT + "figures_out")
 os.makedirs(fig_save_path, exist_ok=True)
 
-# embodiments = ["Hopper", "Walker", "HalfCheetah"]
-#
-# for expert in embodiments:
-#     for learner in embodiments:
-#         if expert == learner:
-#             continue
-#         make_and_save_figure(DATA_PATH, fig_save_path, learner, expert)
+ENVIRONMENT = "Gym"
+TYPE = "eval"
+data_folder = os.path.join(FIGURE_PATH_OUT, ENVIRONMENT, TYPE)
 
-FIGURE_VERSION = "v2"
-DATA_PATH = "/Users/tim/Data/nips_figures/XIRL/"
+embodiments = ["Hopper", "Walker", "HalfCheetah"]
+
+for expert in embodiments:
+    for learner in embodiments:
+        if expert == learner:
+            continue
+        make_and_save_figure(data_folder, fig_save_path, learner, expert, algos=["ours", "baseline"])
+
+## Make Gym ablation studies figures
+os.makedirs(fig_save_path, exist_ok=True)
+
+embodiments_ablation = ["Hopper", "HalfCheetah"]
+
+for expert in embodiments_ablation:
+    for learner in embodiments_ablation:
+        if expert == learner:
+            continue
+        for ablation in ABLATION_IDS:
+            make_and_save_figure(data_folder, fig_save_path, learner, expert, algos=["ours", ablation], is_ablation=True)
+
+
+ENVIRONMENT = "XIRL"
+
+## Make XIRL figures
+data_folder = os.path.join(FIGURE_PATH_OUT, ENVIRONMENT, TYPE)
+
+
 embodiments = ["gripper", "longstick"]
 for learner in embodiments:
     for expert in embodiments:
         if expert == learner:
             continue
-        make_and_save_figure(DATA_PATH, fig_save_path, learner, expert, algos=["ours", "bline"])
+        make_and_save_figure(data_folder, fig_save_path, learner, expert, algos=["ours", "baseline"])
 
 
 
--- utils.py
+++ utils.py
@@ -1,13 +1,26 @@
 import os
 import pandas
 import numpy as np
+from scipy.interpolate import UnivariateSpline
+from scipy.stats import sem
+import copy
+import matplotlib.pyplot as plt
 
 X_MAX = 2500000
-SMOOTHING = 5
+SMOOTHING = 7
 
-
+# EXPERT_DISTANCES = dict(
+#     Hopper=19.31,
+#     Walker=14.0,
+#     HalfCheetah=158.67,
+# )
 
 def find_cutoff(x):
+
+    # this means that data is from the XIRL experiments
+    if x[-1] < 2000000:
+        return len(x)
+
     cutoff = -1
     for i in range(len(x)):
         if x[i+1] > X_MAX:
@@ -18,18 +31,29 @@ def find_cutoff(x):
 
     return cutoff
 
-def smooth_data(y):
-    assert SMOOTHING%2 == 1
-    offset = int((SMOOTHING-1)/2)
+def smooth_data(y, smoothing_factor=None):
 
-    for i in range(offset, len(y)-offset-1):
-        summed = 0
+
+    if smoothing_factor is None:
+        smoothing_factor = SMOOTHING
+
+    assert smoothing_factor % 2 == 1
+    offset = int((smoothing_factor-1)/2)
+
+    try:
+        y_padded = np.concatenate((y[0]*np.ones(offset), y, y[-1]*np.ones(offset)), axis=0)
+    except:
+        import pdb
+        pdb.set_trace()
+
+    for i in range(offset, len(y_padded)-offset-1):
+        accumulated = []
         for j in np.arange(-1*offset, offset+1):
-            summed += y[int(i+j)]
-        average = summed/SMOOTHING
-        y[i] = average
+            accumulated.append(y_padded[int(i+j)])
+        average = np.nanmean(accumulated)
+        y_padded[i] = average
 
-    return y
+    return y_padded[offset:-offset]
 
 
 def get_fname_input(learner, expert):
@@ -40,7 +64,7 @@ def get_fname_output(learner, expert):
     fname = f"{learner}_from_{expert}"
     return fname
 
-def get_figure_fpath(data_to_plot, fig_save_path, learner, expert):
+def get_figure_fpath(fig_save_path, learner, expert):
     fname_base = get_fname_output(learner, expert)
     fname = f"{fname_base}"
 
@@ -53,6 +77,7 @@ def load_data(data_path, learner, expert, algo="ours"):
     fname += ".csv"
     fpath = os.path.join(data_path, algo, fname)
     data_in = pandas.read_csv(fpath)
+    data_in = data_in.fillna(method="backfill")
 
     assert (data_in.shape[1] - 1) % 3 == 0
 
@@ -65,15 +90,16 @@ def compute_mean_std_from_xy_pairs(xy_pairs):
         ys.append(item[1])
 
     ys = np.stack(ys, axis=1)
-    mean = np.mean(ys, axis=1)
-    std = np.std(ys, axis=1)
+    mean = np.median(ys, axis=1)
+    max = np.max(ys, axis=1)
+    std = sem(ys, axis=1, nan_policy="omit")
+    # std = np.nanstd(ys, axis=1)
 
-    return mean, std
+    return mean, std, max
 
 
-def get_xy_pairs(data):
+def get_xy_pairs(data, agent_type):
     n_seeds = int((data.shape[1] - 1) / 6)
-
 
     x_y_pairs = []
 
@@ -84,10 +110,12 @@ def get_xy_pairs(data):
 
         y = smooth_data(y)
 
-        # cutoff = find_cutoff(x)
-        #
-        # x = x[:cutoff]
-        # y = y[:cutoff]
+        cutoff = find_cutoff(x)
+
+        x = x[:cutoff]
+        y = y[:cutoff]
+
+        # y = y/EXPERT_DISTANCES[agent_type]
 
         if len(x_y_pairs) > 0:
             assert x.shape == x_y_pairs[-1][0].shape
@@ -98,24 +126,55 @@ def get_xy_pairs(data):
     return x_y_pairs, n_seeds
 
 
-def process_data(data):
+def process_data(data, agent_type):
 
-    xy_pairs, n_seeds = get_xy_pairs(data)
+    xy_pairs, n_seeds = get_xy_pairs(data, agent_type)
 
-    mean, std = compute_mean_std_from_xy_pairs(xy_pairs)
+    y_margin_max = np.NINF
+    for pair in xy_pairs:
+        y_margin_max = (np.max(pair[1])-np.min(pair[1])) if (np.max(pair[1])-np.min(pair[1])) > y_margin_max else y_margin_max
+
+    mean, std, max = compute_mean_std_from_xy_pairs(xy_pairs)
 
     data_to_plot = dict(
         xy_pairs=xy_pairs,
         mean=mean,
         std=std,
-        n_seeds=n_seeds
+        n_seeds=n_seeds,
+        y_margin_max=y_margin_max,
+        max=max,
+        original_data=data
     )
 
     return data_to_plot
 
-def plot_data(data_to_plot, ax, color):
-    # for pair in data_to_plot["xy_pairs"]:
-    #     ax.plot(pair[0], pair[1])
+def remove_double_entries(x, y):
+
+    for i in range(len(x)-1):
+        if x[i] + 20000 > x[i+1]:
+            average = (y[i] + y[i+1])/2
+            x[i+1] = x[i]
+            y[i] = average
+            y[i+1] = average
+
+    return x, y
+
+
+def plot_data(data_to_plot, ax, color, label, fig, white_line_dist=None):
+
+    # for cur_run in data_to_plot["xy_pairs"]:
+    #     ax.plot(cur_run[0], cur_run[1], color=color, linewidth=0.5, label=label, alpha=0.3)
+    #     renderer = fig.canvas.renderer
+    #     ax.draw(renderer)
+    #     plt.draw()
+
+
+    if "mean" in data_to_plot.keys():
+        x = data_to_plot["xy_pairs"][0][0]
+        mean = data_to_plot["mean"]
+
+        ax.plot(x, mean, color=color, linewidth=0.5, label=label)
+
 
     if "std" in data_to_plot.keys():
         mean = data_to_plot["mean"]
@@ -123,15 +182,63 @@ def plot_data(data_to_plot, ax, color):
         y_lower = mean - std
         y_upper = mean + std
         x = data_to_plot["xy_pairs"][0][0]
-        ax.fill_between(x, y_lower, y_upper, facecolor=color, alpha=0.1)
+        ax.fill_between(x, y_lower, y_upper, facecolor=color, alpha=0.05)
+    #
+    #     # white_line_points = get_white_line_points(x, y_lower, white_line_dist)
+    #
+    #     # plot white lines
+    #     # ax.plot(white_line_points[0], white_line_points[1], color="black", linewidth=0.1)
+    #     # ax.plot(x, y_upper-white_line_dist, color="white", linewidth=0.1)
+    #
+    # ax.legend()
 
-    if "mean" in data_to_plot.keys():
-        x = data_to_plot["xy_pairs"][0][0]
-        mean = data_to_plot["mean"]
-        ax.plot(x, mean, color=color, linewidth=0.5)
 
-def save_fig(plt, data_to_plot, fig_save_path, learner, expert):
-    fpath_base = get_figure_fpath(data_to_plot, fig_save_path, learner, expert)
+def save_fig(plt, fig_save_path, learner, expert, ending=None):
+    fpath_base = get_figure_fpath(fig_save_path, learner, expert)
+
+    if ending is not None:
+        fpath_base += ending
+
     plt.tight_layout()
     plt.savefig(f"{fpath_base}.png")
     plt.savefig(f"{fpath_base}.pgf")
+
+
+def get_white_line_points(x, y_lower, white_line_dist):
+    white_line_dist = 0.02
+
+    assert len(x) == len(y_lower)
+
+    x = x
+
+    y = y_lower
+
+    x_scale = np.max(x) - np.min(x)
+    y_scale = np.max(y) - np.min(y)
+
+    x_min = np.min(x)
+    y_min = np.min(y)
+
+    x = (x-x_min)/x_scale
+    y = (y-y_min)/y_scale
+
+    white_line_points = [[], []]
+
+    for i in range(1, len(y)-1):
+        p = np.array((x[i], y[i]))
+        pb = np.array((x[i-1], y[i-1]))
+        pa = np.array((x[i+1], y[i+1]))
+        line = pa - pb
+        ortho = np.array((-1*line[1], line[0]))
+        ortho = ortho/np.linalg.norm(ortho)
+        p_white_line = p + ortho * white_line_dist
+
+        white_line_points[0].append(p_white_line[0]*x_scale+x_min)
+        white_line_points[1].append(p_white_line[1]*y_scale+y_min)
+
+        # white_line_points[0].append(p_white_line[0])
+        # white_line_points[1].append(p_white_line[1])
+
+
+    return white_line_points
+
